04f3ccdc6318d47723b121ffb67f94ed5a90cc75
Alexander Milchinskiy <milchinskiy@gmail.com>
2023-07-27 15:22:19 +0300

    custom settings

--- config.c
+++ config.c
@@ -4,16 +4,17 @@
 #include "util.h"
 
 Block blocks[] = {
-    {"sb-mail",    600,  1 },
-    {"sb-music",   0,    2 },
-    {"sb-disk",    1800, 3 },
-    {"sb-memory",  10,   4 },
-    {"sb-loadavg", 5,    5 },
-    {"sb-mic",     0,    6 },
-    {"sb-record",  0,    7 },
-    {"sb-volume",  0,    8 },
-    {"sb-battery", 5,    9 },
-    {"sb-date",    1,    10},
+    {"/opt/dwmblocks-async/scripts/kb-layout.sh",       600,    19},
+    {"/opt/dwmblocks-async/scripts/pacman.updates.sh",	6000,   12},
+    {"/opt/dwmblocks-async/scripts/volume.sh",          0,      10},
+    {"/opt/dwmblocks-async/scripts/battery.sh",			300,    0},
+    {"/opt/dwmblocks-async/scripts/cpu.load.sh",		2,	    0},
+    {"/opt/dwmblocks-async/scripts/ram.sh",				2,	    0},
+    {"/opt/dwmblocks-async/scripts/cpu.temp.sh",		2,	    0},
+    {"/opt/dwmblocks-async/scripts/disk.sh /home", 		10,	    0},
+    {"/opt/dwmblocks-async/scripts/weather.sh",         600,    0},
+    {"/opt/dwmblocks-async/scripts/wlan.sh",            5,      18},
+    {"/opt/dwmblocks-async/scripts/clock.sh",			1,      0}
 };
 
 const unsigned short blockCount = LEN(blocks);
