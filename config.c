5b4ee6163c3d5990df9fb09def6ceaac6035b369
Alexander Milchinskiy <milchinskiy@gmail.com>
2023-10-22 18:37:26 +0300

    replace dozen scripts by single one in user scope

--- config.c
+++ config.c
@@ -4,16 +4,17 @@
 #include "util.h"
 
 Block blocks[] = {
-    {"~/.config/dwmblocks/scripts/kb-layout.sh",       600,    19},
-    {"~/.config/dwmblocks/scripts/volume.sh",          0,      10},
-    {"~/.config/dwmblocks/scripts/battery.sh",			300,    0},
-    {"~/.config/dwmblocks/scripts/cpu.load.sh",		2,	    0},
-    {"~/.config/dwmblocks/scripts/ram.sh",				2,	    0},
-    {"~/.config/dwmblocks/scripts/cpu.temp.sh",		2,	    0},
-    {"~/.config/dwmblocks/scripts/disk.sh /home", 		10,	    0},
-    {"~/.config/dwmblocks/scripts/weather.sh",         600,    0},
-    {"~/.config/dwmblocks/scripts/wlan.sh",            5,      18},
-    {"~/.config/dwmblocks/scripts/clock.sh",			1,      0}
+    {"dwmblocks_scripts --keyboard-layout", 600,    19},
+    {"dwmblocks_scripts --pacman-updates",	6000,   12},
+    {"dwmblocks_scripts --volume",          0,      10},
+    {"dwmblocks_scripts --battery",         300,    0},
+    {"dwmblocks_scripts --cpu-load",		2,	    0},
+    {"dwmblocks_scripts --ram",				2,	    0},
+    {"dwmblocks_scripts --cpu-temp",		2,	    0},
+    {"dwmblocks_scripts --disk /home", 		10,	    0},
+    {"dwmblocks_scripts --weather",         600,    0},
+    {"dwmblocks_scripts --wlan",            5,      18},
+    {"dwmblocks_scripts --clock",			1,      0}
 };
 
 const unsigned short blockCount = LEN(blocks);
